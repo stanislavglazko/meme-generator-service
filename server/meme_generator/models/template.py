f9a2032ddee1f893101cd3ca404f43e48112f5d2
stanislavglazko <stanislav.glazko@dualbootpartners.com>
2024-10-05 08:41:55 +0300

    Remove image url field from Meme Template

--- server/meme_generator/models/template.py
+++ server/meme_generator/models/template.py
@@ -3,11 +3,12 @@ from django.db import models
 
 class MemeTemplate(models.Model):
     name = models.CharField(max_length=100)
-    image_url = models.URLField(blank=True)
     default_top_text = models.CharField(max_length=100, blank=True)
     default_bottom_text = models.CharField(max_length=100, blank=True)
     image = models.ImageField(upload_to="templates/", null=True, blank=True)
 
-    def save(self, *args, **kwargs) -> None:
-        self.image_url = self.image.url
-        super().save(*args, **kwargs)
+    @property
+    def image_url(self):
+        if self.image:
+            return self.image.url
+        return ''
